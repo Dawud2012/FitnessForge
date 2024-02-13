0916cf68cd399e3bf17b0acefb54f216e334f4d0
Dawud2012 <dawud92012@hotmail.com>
2024-02-13 15:02:18 +0500

    models

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -13,16 +13,28 @@ class UserFactory extends Factory
     /**
      * Define the model's default state.
      *
+     * 
      * @return array<string, mixed>
      */
     public function definition()
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'is_verifications' => 1,
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'national_id' => $this->faker->numerify('##############'), // "14 Number"
+            'password' => bcrypt('123456'),
             'remember_token' => Str::random(10),
+            'gender' => rand(1, 2),
+            'profile_image' => $this->faker->imageUrl($width = 200, $height = 200),
+            'birth_date' => $this->faker->dateTimeBetween('1990-01-01', '2012-12-31')->format('Y/m/d'), // outputs something like 17/09/2001
+            'last_login_at' => $this->faker->dateTimeBetween('-1 month', '+1 month'),
+            'total_sessions' => rand(80, 120),
+            'remain_session' => rand(10, 80),
+            'city_id' => rand(1, 24),
+            'gym_id' => rand(1, 50),
         ];
     }
 
@@ -33,8 +45,10 @@ class UserFactory extends Factory
      */
     public function unverified()
     {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
+        return $this->state(function (array $attributes) {
+            return [
+                'email_verified_at' => null,
+            ];
+        });
     }
 }
