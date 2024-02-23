b699beb8d5d2e3bee800ddacdfbce4d579eddf61
Dawud2012 <dawud92012@hotmail.com>
2024-02-23 22:47:14 +0500

    console, listeners notifications

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Event;
 class EventServiceProvider extends ServiceProvider
 {
     /**
-     * The event to listener mappings for the application.
+     * The event listener mappings for the application.
      *
      * @var array<class-string, array<int, class-string>>
      */
@@ -18,7 +18,15 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+
+        #=======================================================================================#
+        #			                    for update last login at                              	#
+        #=======================================================================================#        
+        'Illuminate\Auth\Events\Login' => [
+            'App\Listeners\LogSuccessfulLogin',
+        ],
     ];
+
 
     /**
      * Register any events for your application.
